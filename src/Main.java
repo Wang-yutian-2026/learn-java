7e6ec0341123383b16ad82af90143c6e95d3eb03
王宇田 <1754771083@qq.com>
2026-07-23 10:08:55 +0800

    初始化Java学习项目

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        System.out.println("Hello,it's");
+        System.out.println(new Date());
+
+    }
+}
