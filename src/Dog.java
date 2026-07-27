b6cd3b7dbb6c70081231c0d67571b557f3a3e473
王宇田 <1754771083@qq.com>
2026-07-27 09:46:05 +0800

    ==和equals()方法

--- src/Dog.java
+++ src/Dog.java
@@ -11,7 +11,17 @@ public class Dog {
         scruffy.name = "scruffy";
         scruffy.says = "Wurf!";
 
-        System.out.println("狗名字："+spot.name+"，叫声："+spot.says);
+        Dog spot2 = spot;
+
+        System.out.println("==== == 地址比较 ====");
+        System.out.println("spot == spots:"+(spot==spot2));
+        System.out.println("spot == scruffy:"+(spot == scruffy));
+
+        System.out.println("\n==== equals() 比较 ====");
+        System.out.println("spot.equals(spot2):"+spot.equals(spot2));
+        System.out.println("spot.equals(scruff):"+spot.equals(scruffy));
+
+        System.out.println("\n狗名字："+spot.name+"，叫声："+spot.says);
         System.out.println("狗名字："+scruffy.name+"，叫声："+scruffy.says);
     }
 }
