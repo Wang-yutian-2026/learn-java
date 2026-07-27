09ed6eec5bcc4b8c1b72359d429427e186bedd8a
王宇田 <1754771083@qq.com>
2026-07-27 12:54:12 +0800

    枚举类型'

--- src/EnumPaperTest.java
+++ src/EnumPaperTest.java
@@ -4,15 +4,36 @@ enum PaperMoney{
     TEN_YUAN,
     TWENTY_YUAN,
     FIFTY_YUAN,
-    ONE_HUNDRED
+    ONE_HUNDRED_YUAN
 }
 
 public class EnumPaperTest {
     public static void main(String[] args){
-        PaperMoney[] moneyArr = PaperMoney.values();
 
-        for (PaperMoney money : moneyArr){
-            System.out.println("枚举常量："+money+",ordinal序号："+money.ordinal());
+        for (PaperMoney money : PaperMoney.values()){
+            System.out.println("枚举值：："+money+"->描述：");
+            switch (money){
+                case ONE_YUAN:
+                    System.out.println("1元人民币纸币");
+                    break;
+                case FIVE_YUAN:
+                    System.out.println("5元人民币纸币");
+                    break;
+                case TEN_YUAN:
+                    System.out.println("10元人民币纸币");
+                    break;
+                case TWENTY_YUAN:
+                    System.out.println("20元人民币纸币");
+                    break;
+                case FIFTY_YUAN:
+                    System.out.println("50元人民币纸币");
+                    break;
+                case ONE_HUNDRED_YUAN:
+                    System.out.println("100元人民币纸币");
+                    break;
+                default:
+                    System.out.println("未知面值纸币");
+            }
         }
     }
 }
