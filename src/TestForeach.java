c9f8de221d0784a645c549db947fedd7c31dbca1
王宇田 <1754771083@qq.com>
2026-07-27 09:58:50 +0800

    foreach语法

---
+++ src/TestForeach.java
@@ -0,0 +1,13 @@
+public class TestForeach {
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        for (int num : arr){
+            System.out.println(num);
+        }
+
+        String[] dogs = {"spot","scruffy"};
+        for (String name : dogs){
+            System.out.println(name);
+        }
+    }
+}
